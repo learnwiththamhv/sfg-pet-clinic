5b9fc747d7f994b41e3f66df4b6548e922ffb1fe
learnwiththamhv <learnwiththamhv@gmail.com>
2019-10-12 04:37:51 +0700

    implement Map service

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/PetMapService.java
@@ -0,0 +1,40 @@
+package guru.springframework.sfgpetclinic.service.map;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.service.CrudService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by thamhv on 10/12/2019.
+ */
+@Service
+public class PetMapService extends AbstractMapService<Pet,Long> implements CrudService<Pet,Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/VetMapService.java
@@ -0,0 +1,40 @@
+package guru.springframework.sfgpetclinic.service.map;
+
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.service.CrudService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by thamhv on 10/12/2019.
+ */
+@Service
+public class VetMapService extends AbstractMapService<Vet,Long> implements CrudService<Vet,Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
