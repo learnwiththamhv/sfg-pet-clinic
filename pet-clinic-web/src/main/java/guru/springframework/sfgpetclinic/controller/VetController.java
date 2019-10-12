9447c6087916b0881b91ce60f6f35be4b1481fc9
learnwiththamhv <learnwiththamhv@gmail.com>
2019-10-12 13:24:30 +0700

    self-managed Id

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgpetclinic.controller;
 
+import guru.springframework.sfgpetclinic.service.map.VetMapService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,8 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/vets")
 public class VetController {
 
+    private VetMapService vetMapService;
+
+    @Autowired
+    public VetController(VetMapService vetMapService) {
+        this.vetMapService = vetMapService;
+    }
+
     @RequestMapping({"","/","index","index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets",vetMapService.findAll());
+
         return "vets/index";
     }
 }
